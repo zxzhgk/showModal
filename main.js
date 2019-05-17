c00b9bd8715c4934760bc6c8e121404177a27dac
zhangxu <zhangxu@hspotr.cn>
2019-05-17 18:16:08 +0800

    删除一些多余的代码

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex';
+Vue.use(Vuex)
+Vue.config.productionTip = false
 
-// import pageHead from './components/page-head.vue'
-// import pageFoot from './components/page-foot.vue'
-
-// 注册全局modal组件
 import initModal from "@/components/zhangxu-showModal/initModal.js";
 import showModal from '@/components/zhangxu-showModal/show-modal';
 initModal(Vue);
 Vue.component('show-modal',showModal);
 
-import store from './store'
-Vue.config.productionTip = false
-
 App.mpType = 'app'
 
+const store = new Vuex.Store();
 const app = new Vue({
 	store,
-	...App
+    ...App
 })
 app.$mount()
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<view @click="aaa">
+			<text class="title">{{title}}</text>
+		</view>
+		<show-modal></show-modal>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '点我点我'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			aaa(){
+				this.$showModal({
+				  title: '提示',
+				  content: '这是一个模态弹窗',
+				  success(res) {
+					if (res.confirm) {
+					  console.log('用户点击确定')
+					} else if (res.cancel) {
+					  console.log('用户点击取消')
+					}
+				  }
+				})
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		font-size: 30px;
+	}
+</style>
